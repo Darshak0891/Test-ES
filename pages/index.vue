454ac3aa7a71a7ae72f65eb497e90ad721580bc8
Darshak <darshakpatel02@gmail.com>
2023-11-10 18:27:17 +0530

    push

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main-wrapper" class="main-wrapper">
+        <YogaInstructorMain />
+    </div>
+</template>
+<script>
+import YogaInstructorMain from "~~/components/home-yoga-instructor/YogaInstructorMain.vue";
+
+export default {
+    name: "home",
+    components: {
+        YogaInstructorMain
+    },
+    created() {
+        api().get("site-setting-show").then((res) => {
+        if (res.data.success) {
+          this.site = res.data.data;
+        }
+      });
+        // $fetch('https://jsonplaceholder.typicode.com/users')
+    }
+};
+</script>
+<style scoped></style>
